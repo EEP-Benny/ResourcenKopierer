c004507e988dd9d5cd27c6e3a9b2ac4e03032cb8
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2018-11-12 12:21:39 +0100

    Batch script instead of shell script to create Release folder

---
+++ release.cmd
@@ -0,0 +1,14 @@
+SETLOCAL ENABLEEXTENSIONS
+
+SET releasedir=release\ResourcenKopierer
+SET releasefile=%releasedir%.zip
+
+RMDIR /s /q "%releasedir%"
+DEL "%releasefile%"
+MKDIR "%releasedir%"
+XCOPY /i "data" "%releasedir%"
+XCOPY /i "build" "%releasedir%"
+
+explorer "%releasedir%\.."
+
+ECHO The folder %releasedir% needs to be zipped manually
